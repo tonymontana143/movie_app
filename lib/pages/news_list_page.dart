f01274676d26562fdafde04d5c14d38a0fc30729
Maksat, SE - 2218 <virys200543@gmail.com>
2024-05-02 09:31:38 +0500

    Testing

--- lib/pages/news_list_page.dart
+++ lib/pages/news_list_page.dart
@@ -7,11 +7,11 @@ class NewsListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     const Map<String, String> newsImages = {
       'Breaking News 1': 'https://images.thedirect.com/media/article_full/planet-of-the-apes-4.jpg',
-      'Latest Update 2': 'https://images.thedirect.com/media/article_full/planet-of-the-apes-4.jpg',
-      'Top Story 3': 'https://images.thedirect.com/media/article_full/planet-of-the-apes-4.jpg',
-      'Local News 4': 'https://images.thedirect.com/media/article_full/planet-of-the-apes-4.jpg',
-      'Global Events 5': 'https://images.thedirect.com/media/article_full/planet-of-the-apes-4.jpg',
-      'Sports Update 6': 'https://images.thedirect.com/media/article_full/planet-of-the-apes-4.jpg',
+      'Latest Update 2': 'https://m.media-amazon.com/images/M/MV5BZGI4NTEwNTAtZDcwMi00MDkxLTg1OGYtNTZmMzE3ZDljNzVlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg',
+      'Top Story 3': 'https://img.etimg.com/thumb/width-1200,height-900,imgsize-9086,resizemode-75,msid-103845041/news/international/us/when-will-the-kingdom-of-the-planet-of-the-apes-release-heres-what-we-know-so-far.jpg',
+      'Local News 4': 'https://www.rollingstone.com/wp-content/uploads/2018/06/rs-18960-apes-1800-1404228817.jpg',
+      'Global Events 5': 'https://assetsio.gnwcdn.com/Kingdom-of-the-Planet-of-the-Apes_tyrant.jpg?width=1200&height=630&fit=crop&enable=upscale&auto=webp',
+      'Sports Update 6': 'https://i.kinja-img.com/image/upload/c_fit,q_60,w_645/d020dd59b90e8125909fb16afe1ea47f.jpg',
     };
 
     const List<String> newsTitles = [

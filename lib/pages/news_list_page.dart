6b45aaaabd0dbbffe37f0140a232eeea21ac2e94
Maksat, SE - 2218 <virys200543@gmail.com>
2024-05-02 07:49:09 +0500

    Testing

---
+++ lib/pages/news_list_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class NewsListPage extends StatelessWidget {
+  const NewsListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2, // Number of columns in the grid
+        children: List.generate(
+          10, // Number of items in the grid
+              (index) {
+            return Card(
+              margin: const EdgeInsets.all(10),
+              child: Center(
+                child: Text(
+                  'News $index',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}

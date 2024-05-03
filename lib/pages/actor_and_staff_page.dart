a30dbb707c9c52837350f67ffef8f87b667f4b2e
iliias <kenes_2005@mail.ru>
2024-05-03 09:47:38 +0500

    info about actor

--- lib/pages/actor_and_staff_page.dart
+++ lib/pages/actor_and_staff_page.dart
@@ -7,57 +7,87 @@ class ActorStaffPage extends StatelessWidget {
   final String imgUrl;
 
   const ActorStaffPage({
-    super.key,
+    Key? key,
     required this.actorName,
     required this.actorSurname,
     required this.actorRole,
-    required this.imgUrl
-  });
+    required this.imgUrl,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("$actorName $actorSurname", style: const TextStyle(fontFamily: "Poppins"))
+        title: Text("$actorName $actorSurname", style: const TextStyle(fontFamily: "Poppins")),
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 20.0),
         child: Center(
-         child: Column(
-           children: [
-             Image.network(
-               imgUrl,
-               width: 200,
-               height: 200,
-               fit: BoxFit.cover,
-               errorBuilder: (context, error, stackTrace) {
-                 return const Text("Failed to load the image!");
-               },
-             ),
-             Padding(
-               padding: const EdgeInsets.only(top: 10.0),
-               child: Text(
-                 "$actorName $actorSurname",
-                 style: const TextStyle(
-                     fontFamily: "Poppins",
-                   fontSize: 20,
-                   fontWeight: FontWeight.w800
-                 ),
-               ),
-             ),
-             Padding(
-               padding: const EdgeInsets.only(top: 10.0),
-               child: Text(
-                 actorRole,
-                 style: const TextStyle(
-                     fontFamily: "Poppins",
-                     fontSize: 16,
-                     fontWeight: FontWeight.w500
-                 ),
-               ),
-             )
-           ],
-         ),
+          child: Column(
+            children: [
+              Image.network(
+                imgUrl,
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return const Text("Failed to load the image!");
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Text(
+                  "$actorName $actorSurname",
+                  style: const TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Text(
+                  actorRole,
+                  style: const TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  "Biography: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id lectus a nulla pretium scelerisque. Donec efficitur fermentum ultricies.",
+                  style: const TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 14,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  "Birth Date: January 1, 1970",
+                  style: const TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 14,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  "Nationality: USA",
+                  style: const TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

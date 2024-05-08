7905b4b3d30402cad235e0123190d14bb11a20d0
iliias <kenes_2005@mail.ru>
2024-05-08 22:49:23 +0500

    I have added authorization and registration using Mongo Database

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
-import 'pages/movie.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:movie_app/pages/login_page.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(MyApp(token: prefs.getString('token'),));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+  final token;
+  const MyApp({
+    @required this.token,
+    Key? key,
+}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Planet of the Apes',
-      home: MoviePage(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primaryColor: Colors.black,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignInPage(),
     );
   }
 }

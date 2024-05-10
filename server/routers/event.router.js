a026815fa83af2ac09626016208c2da1af0c865b
Maksat, SE - 2218 <virys200543@gmail.com>
2024-05-10 13:49:41 +0500

    .

--- server/routers/event.router.js
+++ server/routers/event.router.js
@@ -14,3 +14,58 @@ router.put('/:id', EventController.updateEvent);
 // Delete
 router.delete('/:id', EventController.deleteEvent);
 module.exports = router;
+
+/*
+paths:
+  /event:
+    post:
+      summary: Create a new event
+      operationId: createEvent
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/Event'
+      responses:
+        '200':
+          description: Successfully created
+    get:
+      summary: Get all events
+      operationId: getEvents
+      responses:
+        '200':
+          description: Successful operation
+    put:
+      summary: Update an existing event
+      operationId: updateEvent
+      parameters:
+        - in: path
+          name: id
+          required: true
+          description: ID of the event to update
+          schema:
+            type: string
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/Event'
+      responses:
+        '200':
+          description: Successfully updated
+    delete:
+      summary: Delete an event
+      operationId: deleteEvent
+      parameters:
+        - in: path
+          name: id
+          required: true
+          description: ID of the event to delete
+          schema:
+            type: string
+      responses:
+        '200':
+          description: Successfully deleted
+*/
